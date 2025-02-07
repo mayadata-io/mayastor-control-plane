0ba1e326d2481eaace874f733ab1a459c2249030
Tiago Castro <tiagolobocastro@gmail.com>
2025-02-07 15:07:18 +0000

    fix(ha/target/shutdown): reshutdown current target
    When the only remaining healthy replica is local to a failed shutdown target,
    the volume will fail to republish.
    In this case, ensure we confirm the shutdown of the target, which will allow
    the volume to be re-published.
    
    Adds a test which simulates this condition.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- control-plane/agents/src/bin/core/volume/operations.rs
+++ control-plane/agents/src/bin/core/volume/operations.rs
@@ -498,7 +498,10 @@ impl ResourcePublishing for OperationGuardArc<VolumeSpec> {
         let mut older_nexus = specs.nexus(target_cfg.target().nexus()).await?;
         let mut move_nexus = true;
         let mut nexus_node = None;
-        match healthy_volume_replicas(&spec, &older_nexus.as_ref().node, registry).await {
+        let healthy_replicas_result =
+            healthy_volume_replicas(&spec, &older_nexus.as_ref().node, registry).await;
+        let healthy_replicas = healthy_replicas_result.is_ok();
+        match healthy_replicas_result {
             Ok(_) => {
                 let reuse_existing = match request.reuse_existing_fallback
                     && !request.reuse_existing
@@ -560,7 +563,10 @@ impl ResourcePublishing for OperationGuardArc<VolumeSpec> {
 
         // Shutdown the older nexus before newer nexus creation.
         let result = older_nexus
-            .shutdown(registry, &ShutdownNexus::new(older_nexus_id, true))
+            .shutdown(
+                registry,
+                &ShutdownNexus::new(older_nexus_id, true, !healthy_replicas),
+            )
             .await;
         self.validate_update_step(registry, result, &spec_clone)
             .await?;
